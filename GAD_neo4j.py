6dc373ecdd25cb1174cdba584f06ba99a54be2c1
Sreevatsa03 <sreevatsa.nukala@gmail.com>
2023-04-10 14:39:19 -0400

    added degree centrality and node info

--- GAD_neo4j.py
+++ GAD_neo4j.py
@@ -101,9 +101,9 @@ class GADGraph():
             for record in result:
                 print(record)
 
-    def get_relationships(self, node_idx):
+    def get_associations(self, node_idx):
         """
-        Get all relationships for a given disease or gene
+        Get all associations for a given disease or gene
         :param node_idx: index of gene or disease node
         :type node_idx: str
         """
@@ -133,10 +133,192 @@ class GADGraph():
             if not result.peek():
                 print("No gene or disease node with index", str(node_idx), "found.")
                 return
+            else:
+                for record in result:
+                    print(record)
+    
+    def get_gene_details(self, gene):
+        """
+        Get details of a gene from its geneId or geneSymbol
+
+        :param gene: geneId or geneSymbol
+        :type gene: str
+        :return: geneSymbol or geneId
+        :rtype: str
+        """
+
+        # get details of a gene from its geneId
+        geneId_query = """
+        MATCH (g:Gene {geneId: $gene})
+        RETURN g.geneSymbol as geneSymbol, g.DSI AS DSI, g.DPI AS DPI, g.PLI AS PLI, g.protein_class_name AS protein_class_name, g.NofDiseases AS NofDiseases
+        """
+
+        # get details of a gene from its geneSymbol
+        geneSymbol_query = """
+        MATCH (g:Gene {geneSymbol: $gene})
+        RETURN g.geneId as geneId, g.DSI AS DSI, g.DPI AS DPI, g.PLI AS PLI, g.protein_class_name AS protein_class_name, g.NofDiseases AS NofDiseases
+        """
+
+        with self._driver.session() as session:
+            result = session.run(geneId_query, gene=str(gene))
+            
+            # check if results is empty
+            if not result.peek():
+                result = session.run(geneSymbol_query, gene=str(gene))
+            else:
+                for record in result:
+                    print(record)
+
+                    # return geneSymbol
+                    return record['geneSymbol']
+
+            # check if results is empty
+            if not result.peek():
+                print("No gene with index or symbol", str(gene), "found.")
+                return
+            else:
+                for record in result:
+                    print(record)
+
+                    # return geneId
+                    return record['geneId']
+
+    def get_disease_details(self, disease):
+        """
+        Get details of a disease from its diseaseId or diseaseName
+
+        :param disease: diseaseId or diseaseName
+        :type disease: str
+        :return: diseaseName or diseaseId
+        :rtype: str
+        """
+
+        # get details of a disease from its diseaseId
+        diseaseId_query = """
+        MATCH (d:Disease {diseaseId: $disease})
+        RETURN d.diseaseName as diseaseName, d.diseaseSemanticType as diseaseSemanticType, d.NofGenes AS NofGenes
+        """
+
+        # get details of a disease from its diseaseName
+        diseaseName_query = """
+        MATCH (d:Disease {diseaseName: $disease})
+        RETURN d.diseaseId as diseaseId, d.diseaseSemanticType as diseaseSemanticType, d.NofGenes AS NofGenes
+        """
+
+        with self._driver.session() as session:
+            result = session.run(diseaseId_query, disease=str(disease))
+            
+            # check if results is empty
+            if not result.peek():
+                result = session.run(diseaseName_query, disease=str(disease))
+            else:
+                for record in result:
+                    print(record)
+
+                    # return diseaseName
+                    return record['diseaseName']
+
+            # check if results is empty
+            if not result.peek():
+                print("No disease with index or name", str(disease), "found.")
+                return
+            else:
+                for record in result:
+                    print(record)
+
+                    # return diseaseId
+                    return record['diseaseId']
                 
+    def graph_projection(self):
+        """
+        Project graph onto genes and diseases.
+        """
+
+        query = """
+        CALL gds.graph.create.cypher(
+            'gad',
+            'MATCH (g:Gene) RETURN id(g) AS id',
+            'MATCH (g:Gene)-[r:ASSOCIATION]-(d:Disease) RETURN id(g) AS source, id(d) AS target'
+        )
+        """
+
+        with self._driver.session() as session:
+            result = session.run(query)
             for record in result:
                 print(record)
 
+    def drop_graph_projection(self):
+        """
+        Drop graph.
+        """
+
+        query = """
+        CALL gds.graph.drop('gad')
+        """
+
+        with self._driver.session() as session:
+            result = session.run(query)
+            for record in result:
+                print(record)
+                
+    def genes_degree_centraility(self):
+        """
+        Get degree centrality for all genes in the graph.
+        """
+
+        query = """
+        CALL gds.degree.stream('gad')
+        YIELD nodeId, score
+        RETURN gds.util.asNode(nodeId).geneSymbol as geneSymbol, score AS score
+        ORDER BY score DESC, geneSymbol DESC
+        """
+
+        with self._driver.session() as session:
+            result = session.run(query)
+
+            gene_count = 0
+            for record in result:
+
+                # check if geneSymbol is not null
+                if record['geneSymbol'] is not None:
+                    print(record)
+
+                    # count genes
+                    gene_count += 1
+                
+                if gene_count == 10:
+                    break
+
+    def disease_degree_centraility(self):
+        """
+        Get degree centrality for all diseases in the graph.
+        """
+
+        query = """
+        CALL gds.degree.stream('gad')
+        YIELD nodeId, score
+        RETURN gds.util.asNode(nodeId).diseaseName as diseaseName, score AS score
+        ORDER BY score DESC, diseaseName DESC
+        """
+
+        with self._driver.session() as session:
+            result = session.run(query)
+
+            disease_count = 0
+            for record in result:
+
+                # check if diseaseName is not null
+                if record['diseaseName'] is not None:
+                    print(record)
+
+                    # count diseases
+                    disease_count += 1
+
+                if disease_count == 10:
+                    break
+
+
+                
     def show_graph(self):
         """
         Show the graph in Neo4j database.
@@ -153,45 +335,34 @@ class GADGraph():
             result = session.run(query)
             for record in result:
                 print(record)
-    
-    # def find_gene(self, gene):
-    #     with self.driver.session() as session:
-    #         result = session.run("MATCH (g:Gene {name: $gene}) RETURN g.name AS name", gene=gene)
-    #         return [record["name"] for record in result]
-
-    # def find_disease(self, disease):
-    #     with self.driver.session() as session:
-    #         result = session.run("MATCH (d:Disease {name: $disease}) RETURN d.name AS name", disease=disease)
-    #         return [record["name"] for record in result]
-
-    # def find_gene_disease(self, gene, disease):
-    #     with self.driver.session() as session:
-    #         result = session.run("MATCH (g:Gene {name: $gene})-[r:ASSOCIATION]->(d:Disease {name: $disease}) RETURN g.name AS gene, d.name AS disease, r.evidence AS evidence, r.score AS score", gene=gene, disease=disease)
-    #         return [record["gene"] for record in result]
-
-    # def find_gene_disease_all(self, gene, disease):
-    #     with self.driver.session() as session:
-    #         result = session.run("MATCH (g:Gene {name: $gene})-[r:ASSOCIATION]->(d:Disease {name: $disease}) RETURN g.name AS gene, d.name AS disease, r.evidence AS evidence, r.score AS score", gene=gene, disease=disease)
-    #         return [record for record in result]
-
-    # def find_gene_disease_all_evidence(self, gene, disease):
-    #     with self.driver.session() as session:
-    #         result = session.run("MATCH (g:Gene {name: $gene})-[r:ASSOCIATION]->(d:Disease {name: $disease}) RETURN g.name AS gene, d.name AS disease, r.evidence AS evidence, r.score AS score", gene=gene, disease=disease)
-    #         return [record["evidence"] for record in result]
-
 
 def main():
     # create GADGraph object
     gad = GADGraph("bolt://localhost:7687", "neo4j", "password")
 
-    # # clear the database
+    # clear the database
     # gad.clear_database()
 
-    # # insert nodes and relationships
+    # insert nodes and relationships
     # gad.insert_nodes_and_relationships()
 
-    # get relationships for a given node
-    gad.get_relationships("19")
+    # get gene details
+    # geneID = gad.get_gene_details("ABCA1")
+
+    # get disease details
+    # diseaseID = gad.get_disease_details("Tangier Disease")
+
+    # get associations for a given node
+    # gad.get_associations(geneID)
+
+    # get associations for a given node
+    # gad.get_associations(diseaseID)
+
+    # genes degree centrality
+    # gad.genes_degree_centraility()
+
+    # diseases degree centrality
+    gad.disease_degree_centraility()
 
     # close the connection
     gad.close()
