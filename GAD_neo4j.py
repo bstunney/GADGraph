db942fd4b9a26e85bb7201dbc7579832472ef73a
Sreevatsa03 <sreevatsa.nukala@gmail.com>
2023-04-17 12:56:42 -0400

    added missing query functionality from python into cypher

--- GAD_neo4j.py
+++ GAD_neo4j.py
@@ -101,16 +101,19 @@ class GADGraph():
             for record in result:
                 print(record)
 
-    def get_associations(self, node_idx):
+    def get_associations(self, node_idx, score_threshold=0.0):
         """
         Get all associations for a given disease or gene
         :param node_idx: index of gene or disease node
         :type node_idx: str
+        :param score_threshold: score threshold for association, defaults to 0.0
+        :type score_threshold: float, optional
         """
 
         # query to check if node_idx matches a gene node's geneId
         gene_query = """
         MATCH (g:Gene {geneId: $node_idx})-[r:ASSOCIATION]-(d:Disease)
+        WHERE toFloat(r.score) >= $score
         RETURN g.geneSymbol as geneSymbol, r.score AS score, d.diseaseName AS diseaseName
         ORDER BY score DESC
         """
@@ -118,16 +121,17 @@ class GADGraph():
         # query to check if node_idx matches a disease node's diseaseId
         disease_query = """
         MATCH (d:Disease {diseaseId: $node_idx})-[r:ASSOCIATION]-(g:Gene)
+        WHERE toFloat(r.score) >= $score
         RETURN d.diseaseName as diseaseName, r.score AS score, g.geneSymbol AS geneSymbol
         ORDER BY score DESC
         """
 
         with self._driver.session() as session:
-            result = session.run(gene_query, node_idx=str(node_idx))
+            result = session.run(gene_query, node_idx=str(node_idx), score=score_threshold)
 
             # check if results is empty
             if not result.peek():
-                result = session.run(disease_query, node_idx=str(node_idx))
+                result = session.run(disease_query, node_idx=str(node_idx), score=score_threshold)
 
             # check if results is empty
             if not result.peek():
@@ -229,96 +233,240 @@ class GADGraph():
                     # return diseaseId
                     return record['diseaseId']
                 
-    def graph_projection(self):
+    def _reverse_graph_projection(self, node_type):
         """
-        Project graph onto genes and diseases.
+        Project reverse graph onto genes or diseases.
+
+        :param node_type: node type to project onto
+        :type node_type: str
         """
 
+        # project reverse graph onto genes or diseases
         query = """
-        CALL gds.graph.create.cypher(
-            'gad',
-            'MATCH (g:Gene) RETURN id(g) AS id',
-            'MATCH (g:Gene)-[r:ASSOCIATION]-(d:Disease) RETURN id(g) AS source, id(d) AS target'
+        CALL gds.graph.project.cypher(
+            'reverse_gad',
+            $node_type,
+            {
+                ASSOCIATION: {
+                    orientation: 'REVERSE',
+                    properties: 'score'
+                }
+            }
         )
         """
 
         with self._driver.session() as session:
-            result = session.run(query)
-            for record in result:
-                print(record)
+            session.run(query, node_type=str(node_type))
+            # for record in result:
+            #     print(record)
 
-    def drop_graph_projection(self):
+    def _drop_graph_projection(self, graph_name):
         """
-        Drop graph.
+        Drop graph if it exists.
+
+        :param graph_name: name of graph to drop
+        :type graph_name: str
         """
 
+        # check if graph exists
+        exists_query = """
+        CALL gds.graph.exists($graph_name)
+            YIELD exists
+        RETURN exists
+        """
+
+        # drop graph if it exists
         query = """
-        CALL gds.graph.drop('gad')
+        CALL gds.graph.drop($graph_name)
         """
 
         with self._driver.session() as session:
-            result = session.run(query)
+
+            # check if graph exists
+            result = session.run(exists_query, graph_name=str(graph_name))
             for record in result:
-                print(record)
+                if record['exists']:
+                    print("Graph", graph_name, "exists. Dropping graph projection.")
+                    result = session.run(query, graph_name=str(graph_name))
+                    for record in result:
+                        print(record)
+                else:
+                    print("Graph", graph_name, "does not exist.")
                 
     def genes_degree_centraility(self):
         """
         Get degree centrality for all genes in the graph.
         """
 
+        # drop graph if it exists
+        self._drop_graph_projection('reverse_gad')
+
+        # project reverse graph onto genes
+        self._reverse_graph_projection('Gene')
+
         query = """
-        CALL gds.degree.stream('gad')
+        CALL gds.degree.stream('reverse_gad')
         YIELD nodeId, score
+        WHERE gds.util.asNode(nodeId).geneSymbol IS NOT NULL
         RETURN gds.util.asNode(nodeId).geneSymbol as geneSymbol, score AS score
         ORDER BY score DESC, geneSymbol DESC
+        LIMIT 10
         """
 
         with self._driver.session() as session:
             result = session.run(query)
 
-            gene_count = 0
             for record in result:
-
-                # check if geneSymbol is not null
-                if record['geneSymbol'] is not None:
-                    print(record)
-
-                    # count genes
-                    gene_count += 1
-                
-                if gene_count == 10:
-                    break
+                print(record)
 
     def disease_degree_centraility(self):
         """
         Get degree centrality for all diseases in the graph.
         """
 
+        # drop graph if it exists
+        self._drop_graph_projection('reverse_gad')
+
+        # project reverse graph onto diseases
+        self._reverse_graph_projection('Disease')
+
         query = """
         CALL gds.degree.stream('gad')
         YIELD nodeId, score
+        WHERE gds.util.asNode(nodeId).diseaseName IS NOT NULL
         RETURN gds.util.asNode(nodeId).diseaseName as diseaseName, score AS score
         ORDER BY score DESC, diseaseName DESC
+        LIMIT 10
         """
 
         with self._driver.session() as session:
             result = session.run(query)
 
-            disease_count = 0
             for record in result:
+                print(record)
 
-                # check if diseaseName is not null
-                if record['diseaseName'] is not None:
-                    print(record)
+    def find_common_dieases(self, gene1, gene2, score1=0.0, score2=0.0):
+        """
+        Find common diseases between two genes.
 
-                    # count diseases
-                    disease_count += 1
+        :param gene1: geneId of gene 1
+        :type gene1: str
+        :param gene2: geneId of gene 2
+        :type gene2: str
+        :param score1: score threshold for gene 1
+        :type score1: float
+        :param score2: score threshold for gene 2
+        :type score2: float
+        :return: common diseases between gene 1 and gene 2
+        :rtype: `Result`
+        """
 
-                if disease_count == 10:
-                    break
+        query = """
+        MATCH (g1:Gene {geneId: $gene1})
+        MATCH (g2:Gene {geneId: $gene2})
+        MATCH (g1)-[r1:ASSOCIATION]->(d:Disease)
+        MATCH (g2)-[r2:ASSOCIATION]->(d)
+        WHERE toFloat(r1.score) >= $score1 AND toFloat(r2.score) >= $score2
+        RETURN d.diseaseName as diseaseName, r1.score as score1, r2.score as score2
+        """
 
+        with self._driver.session() as session:
+            result = session.run(query, gene1=str(gene1), gene2=str(gene2), score1=float(score1), score2=float(score2))
 
-                
+            for record in result:
+                print(record)
+
+            return result
+        
+    def find_common_genes(self, disease1, disease2, score1=0.0, score2=0.0):
+        """
+        Find common genes between two diseases.
+
+        :param disease1: diseaseId of disease 1
+        :type disease1: str
+        :param disease2: diseaseId of disease 2
+        :type disease2: str
+        :param score1: score threshold for disease 1
+        :type score1: float
+        :param score2: score threshold for disease 2
+        :type score2: float
+        :return: common genes between disease 1 and disease 2
+        :rtype: `Result`
+        """
+
+        query = """
+        MATCH (d1:Disease {diseaseId: $disease1})
+        MATCH (d2:Disease {diseaseId: $disease2})
+        MATCH (d1)-[r1:ASSOCIATION]->(g:Gene)
+        MATCH (d2)-[r2:ASSOCIATION]->(g)
+        WHERE toFloat(r1.score) >= $score1 AND toFloat(r2.score) >= $score2
+        RETURN g.geneSymbol as geneSymbol, r1.score as score1, r2.score as score2
+        """
+
+        with self._driver.session() as session:
+            result = session.run(query, disease1=str(disease1), disease2=str(disease2), score1=float(score1), score2=float(score2))
+
+            for record in result:
+                print(record)
+
+            return result
+        
+    def most_connected_genes(self, score=0.0, limit=5):
+        """
+        Find most connected genes in the graph.
+
+        :param score: score threshold
+        :type score: float
+        :param limit: number of results to return
+        :type limit: int
+        :return: most connected genes in the graph
+        :rtype: `Result`
+        """
+
+        query = """
+        MATCH (g:Gene)-[r:ASSOCIATION]->(d:Disease)
+        WHERE toFloat(r.score) >= $score
+        RETURN g.geneSymbol as geneSymbol, count(d) as count
+        ORDER BY count DESC, geneSymbol DESC
+        LIMIT $limit
+        """
+
+        with self._driver.session() as session:
+            result = session.run(query, score=float(score), limit=int(limit))
+
+            for record in result:
+                print(record)
+
+            return result
+    
+    def most_connected_diseases(self, score=0.0, limit=5):
+        """
+        Find most connected diseases in the graph.
+
+        :param score: score threshold
+        :type score: float
+        :param limit: number of results to return
+        :type limit: int
+        :return: most connected diseases in the graph
+        :rtype: `Result`
+        """
+
+        query = """
+        MATCH (d:Disease)-[r:ASSOCIATION]->(g:Gene)
+        WHERE toFloat(r.score) >= $score
+        RETURN d.diseaseName as diseaseName, count(g) as count
+        ORDER BY count DESC, diseaseName DESC
+        LIMIT $limit
+        """
+
+        with self._driver.session() as session:
+            result = session.run(query, score=float(score), limit=int(limit))
+
+            for record in result:
+                print(record)
+
+            return result
+      
     def show_graph(self):
         """
         Show the graph in Neo4j database.
@@ -362,7 +510,10 @@ def main():
     # gad.genes_degree_centraility()
 
     # diseases degree centrality
-    gad.disease_degree_centraility()
+    # gad.disease_degree_centraility()
+
+    # find common diseases between two genes
+    # gad.find_common_dieases('24', '1406', 0.0, 0.0)
 
     # close the connection
     gad.close()
